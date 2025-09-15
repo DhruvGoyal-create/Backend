a0a0cb61a283b43a584cae4b8e7a59d21177c1ff
Dinkr <goyaldhruvgzb@gmail.com>
2025-09-16 00:07:29 +0530

    Add files via upload

---
+++ Back End/Java/Sample Space/Lvl-1/Unit 1/1.java
@@ -0,0 +1,5 @@
+public class Unit_1 {
+    public static void main(String [] args) throws Exception{
+        System.out.println(x:"Hello World");
+    }
+} 
---
+++ Back End/Python Course/Python3.13.3/second.py
@@ -0,0 +1,8 @@
+
+a = int(input("first no."))
+
+b = int(input("second no."))
+
+c = a + b 
+
+print ("C is ",c)
