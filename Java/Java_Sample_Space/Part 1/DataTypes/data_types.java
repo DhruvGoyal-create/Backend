fd685afa772fd3288da0fdc1e6e410373492fa70
Dhruv Goyal <goyaldhruvgzb@gmail.com>
2025-09-16 22:14:23 +0530

    Added Java programs

---
+++ Java/Java_Sample_Space/Part 1/DataTypes/data_types.java
@@ -0,0 +1,26 @@
+public class data_types {
+    public static void main(String[] args) {
+        // Primitive data types
+        byte b = 10;
+        short s = 200;
+        int i = 1000;
+        long l = 100000L;
+        float f = 12.34f;
+        double d = 123.456;
+        char c = 'Z';
+        boolean bool = false;
+
+        // Non-primitive data type
+        String str = "Hello, World!";
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+        System.out.println("String: " + str);
+    }
+}
---
+++ Python Course/Python3.13.3/if eelse.py
@@ -0,0 +1,8 @@
+a=int(input())
+if a%2==0:
+    print("even")
+    print("number")
+
+else :
+    print("odd")
+    print("ok")
