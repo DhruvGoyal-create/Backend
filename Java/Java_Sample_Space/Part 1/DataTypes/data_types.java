1575de877906433503f0c95656837cc1f8b87629
Dhruv Goyal <goyaldhruvgzb@gmail.com>
2025-09-17 04:41:48 +0530

    Added Java programs

--- Java/Java_Sample_Space/Part 1/DataTypes/data_types.java
+++ Java/Java_Sample_Space/Part 1/DataTypes/data_types.java
@@ -1,17 +1,45 @@
 public class data_types {
     public static void main(String[] args) {
-        // Primitive data types
+    
+    // Primitive data types
+    
         byte b = 10;
-        short s = 200;
-        int i = 1000;
-        long l = 100000L;
-        float f = 12.34f;
-        double d = 123.456;
-        char c = 'Z';
-        boolean bool = false;
+        // byte is used to save memory in large arrays, mainly in place of integers
+        // byte data type is 8-bit signed two's complement integer
 
-        // Non-primitive data type
+        short s = 200;
+        // short is also used to save memory in large arrays, mainly in place of integers
+        // short data type is 16-bit signed two's complement integer
+
+        int i = 1000;
+        // int is the most commonly used data type
+        // int data type is 32-bit signed two's complement integer
+
+        long l = 100000L;
+        // long data type is used when int is not large enough
+        // long data type is 64-bit signed two's complement integer
+
+        float f = 12.34f;
+        // float data type is used to save memory in large arrays of floating point numbers
+        // float data type is 32-bit IEEE 754 floating point
+
+        double d = 123.456;
+        // double data type is generally used as the default data type for decimal values
+        // double data type is 64-bit IEEE 754 floating point
+
+        char c = 'Z';
+        // char data type is used to store a single character
+        // char data type is 16-bit Unicode character
+
+        boolean bool = false;
+        // boolean data type is used to store true or false values
+        // boolean data type is not precisely defined in terms of size
+
+    // Non-primitive data type
+        
         String str = "Hello, World!";
+        // String is used to store a sequence of characters
+        // String is not a primitive data type, it is a class in Java
 
         System.out.println("byte: " + b);
         System.out.println("short: " + s);
---
+++ Java/Java_Sample_Space/Part 1/Variables_Operators/variable.java
@@ -0,0 +1,14 @@
+public class variable {
+    public static void main(String[] args) {
+        // Variables are used to store data values in a program.
+        // Each variable has a type, which determines what kind of data it can hold.
+        
+        int age = 25;           // integer variable
+        double salary = 50000;  // double variable
+        String name = "Alice";  // string variable
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
